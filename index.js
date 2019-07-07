5518ce115d2e6e059aa0c071d9738e2815423750
Piyush Kumar <piykumar85@gmail.com>
2019-07-08 00:27:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import JavascriptMcqTest from './mcqQuestions.js';
+import './index.css';
+
+ReactDOM.render(
+	<JavascriptMcqTest />,
+	document.getElementById('root')
+);
